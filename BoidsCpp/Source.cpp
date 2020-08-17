2f1e46de4f65d55830d98eceacd384e70f7b9f10
Antiochian <trin3161@ox.ac.uk>
2020-08-17 18:27:15 +0100

    Implemented spatial partitioning data structure

--- BoidsCpp/Source.cpp
+++ BoidsCpp/Source.cpp
@@ -12,7 +12,7 @@ constexpr int Nx = 960;
 constexpr int Ny = 480;
 constexpr int pixelscale = 1;
 
-const int grid_size = 50;
+const int grid_size = 100;
 const int bar_width = 1;
 int Gx; //number of grid squares across (determined in gen_grid func)
 int Gy;
@@ -20,7 +20,7 @@ int Gy;
 class Boid {
 public:
 	static std::vector<Boid> boid_list; //list of all agents
-	static std::map< std::pair<int, int>, std::vector<Boid> > grid_table; //spatially partitioned agent map
+	static std::map< std::pair<int, int>, std::vector<Boid*> > grid_table; //spatially partitioned agent map
 	
 	int m_id; //agent id
 	olc::vf2d m_pos; //agent position
@@ -28,12 +28,12 @@ public:
 	float m_heading;
 	float m_heading_target;
 
-
 	olc::vf2d m_vel;
 	olc::vf2d m_accel;
 
 	int m_size;
 	olc::Pixel m_col; //color of boid
+
 private:
 	float m_max_force; //constants that determine motion characteristics
 	float m_max_speed;
@@ -49,14 +49,22 @@ private:
 	int m_gy;
 public:
 	Boid(int id) {
-		int m_id = id;
+		m_id = id;
+		m_col = olc::BLUE;
+		if (m_id == 0) { m_col = olc::RED; }
+
+		srand(id); //good for debugging, but change this to a proper RNG later
 		m_size = 5;
+		m_max_speed = 100.0f * (0.75f + 0.5f * static_cast<float> (rand()) / static_cast<float> (RAND_MAX)); //give it some fuzz
+
 		//random position
 		olc::vf2d startpos(static_cast <float> (rand()) / static_cast <float> (RAND_MAX / Nx), static_cast <float> (rand()) / static_cast <float> (RAND_MAX / Ny));
-		m_col = olc::BLUE;
+		m_pos = startpos;
+
+		olc::vf2d startvel(static_cast <float> (rand()) / static_cast <float> (RAND_MAX / m_max_speed), static_cast <float> (rand()) / static_cast <float> (RAND_MAX / m_max_speed));
+		m_vel = startvel;
 
 		m_max_force = 0.6;
-		m_max_speed = 100.0f * (0.75f + 0.5f * static_cast<float> (rand()) / static_cast<float> (RAND_MAX)); //give it some fuzz
 		m_max_turnspeed = 3; //about 0.5 rev/s
 		m_repulse_strength = 2400; //might wanna check this, chief
 		m_equil_dist = 4;
@@ -65,29 +73,44 @@ public:
 
 		m_vision = (float)grid_size;
 
-		//set grid coordinates
-		/*self.gx,self.gy = self.pos//grid_size #current grid cell (goes from -1 to 8)
-        if self.gx < 0:
-            self.gx = Gx
-        elif self.gx > Gx:
-            self.gx = 0
-        if self.gy < 0:
-            self.gy = Gy
-        elif self.gy > Gy:
-            self.gy = 0*/
-
 		m_gx = (int)(m_pos.x / grid_size);
 		m_gy = (int)(m_pos.y / grid_size);
 		//toroidal rollover
 		if (m_gx < 0) { m_gx = Gx; }
-		if (m_gx > 0) { m_gx = 0; }
+		if (m_gx > Gx) { m_gx = 0; }
 		if (m_gy < 0) { m_gy = Gy; }
-		if (m_gy > 0) { m_gy = 0; }
+		if (m_gy > Gy) { m_gy = 0; }
 
 	}
 
 	static void gen_table(){
 		//take boid_list and apply spatial partitioning
+		Boid::grid_table.clear();
+		float tx;
+		float ty;
+		std::pair<int, int> key;
+		for (int i = 0; i < Boid::boid_list.size(); i++) {
+			tx = boid_list[i].m_gx;
+			ty = boid_list[i].m_gy;
+			key = std::make_pair(tx, ty);
+			Boid* ptr = &(Boid::boid_list[i]);
+			
+			if (Boid::grid_table.find(key) == grid_table.end()) {
+				//if key not found then make new vec and add it to map
+				std::vector<Boid*> t_vec({ ptr });
+				//std::pair< std::pair<int,int>, std::vector<Boid*> > (key, t_vec)
+				Boid::grid_table.insert(std::pair< std::pair<int, int>, std::vector<Boid*> >(key, t_vec));
+			}
+			else {
+				Boid::grid_table[key].push_back(ptr); //add to grid
+			}
+			/*if key in grid_table:
+				make new vector
+				insert pointer
+				add to grid_table with key
+			else:
+				add pointer to vector*/
+		}
 	}
 
 	void update(float fElapsedTime) {
@@ -97,7 +120,6 @@ public:
 		if (m_vel.mag2() > m_max_speed * m_max_speed) {
 			m_vel = m_max_speed * m_vel.norm();
 		}
-		LOG(m_vel);
 		m_accel = olc::vf2d(0, 0);
 		//clamp position
 		if (m_pos.x < 0) { m_pos.x = Nx; }
@@ -106,6 +128,11 @@ public:
 		else if (m_pos.y > Ny) { m_pos.y = 0; }
 		//m_pos.x = ((int)m_pos.x) % Nx;
 		//m_pos.y = ((int)m_pos.y) % Ny;
+
+		m_gx = m_pos.x / grid_size;
+		m_gy = m_pos.y / grid_size;
+
+		if (m_id == 0){ std::cout << "G: (" << m_gx << ", " << m_gy << ")\n"; }
 	}
 
 	std::vector<Boid> find_neighbours(int rg = 1){
@@ -113,14 +140,14 @@ public:
 	}
 
 	void calculate_movement() {
-		m_accel = olc::vf2d(0, -10);
+		//m_accel = olc::vf2d(0, -10);
 		//pass
 	}
 };
 
 
 std::vector<Boid> Boid::boid_list;
-std::map< std::pair<int, int>, std::vector<Boid> > Boid::grid_table; //initialise static var
+std::map< std::pair<int, int>, std::vector<Boid*> > Boid::grid_table; //initialise static var
 
 class BoidsApp : public olc::PixelGameEngine
 {
@@ -133,7 +160,16 @@ public:
 
 public:
 	void place_agents() {
-		//initialise grid
+		//temp debug boids
+		Boid loner(0);
+		loner.m_pos = olc::vf2d(0, Ny / 2);
+		loner.m_vel = olc::vf2d(40,0);
+		Boid::boid_list.push_back(loner);
+
+		Boid pal(1);
+		pal.m_pos = olc::vf2d(Nx, Ny / 2);
+		pal.m_vel = olc::vf2d(-40, 0);
+		Boid::boid_list.push_back(pal);
 		
 	}
 
@@ -164,25 +200,21 @@ public:
 		place_agents();
 		Boid::gen_table();
 		// Called once at the start, so create things here
-		//debug loner
 
-		Boid loner(0);
-		loner.m_pos = olc::vf2d(Nx / 2, Ny / 2);
-		loner.m_vel = olc::vf2d(40,40);
-		Boid::boid_list.push_back(loner);
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		Boid::gen_table();
 		Clear(olc::BLACK);
 		//draw gridlines first
-		//for (int i = 0; i < Gy+1; i++) {
-		//	DrawLine({ 0, i * grid_size }, { Nx, i * grid_size }, olc::GREY); //horiz lines
-		//}
-		//for (int i = 0; i < Gx+1; i++) {
-		//	DrawLine({ i * grid_size, 0 }, { i * grid_size, Ny }, olc::GREY); //vert lines
-		//}
+		for (int i = 0; i < Gy+1; i++) {
+			DrawLine({ 0, i * grid_size }, { Nx, i * grid_size }, olc::GREY); //horiz lines
+		}
+		for (int i = 0; i < Gx+1; i++) {
+			DrawLine({ i * grid_size, 0 }, { i * grid_size, Ny }, olc::GREY); //vert lines
+		}
 
 		for (int i = 0; i < Boid::boid_list.size(); i++) {
 			Boid* candidate = &Boid::boid_list[i];
