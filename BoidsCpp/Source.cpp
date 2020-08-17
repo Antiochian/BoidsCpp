c94a135fc9362af39d16f40e1a1bab47a9901039
Antiochian <henry.griffiths@trinity.ox.ac.uk>
2020-08-17 20:59:55 +0100

    fixed bug in timestep regulation

--- BoidsCpp/Source.cpp
+++ BoidsCpp/Source.cpp
@@ -1,18 +1,33 @@
 #define OLC_PGE_APPLICATION
 #define LOG(x) (std::cout << x << std::endl) //homebrew logger
+#define _USE_MATH_DEFINES
+
 #include "olcPixelGameEngine.h"
+
+#include <iterator>
 #include <vector>
 #include <tuple>
 #include <limits>
 #include <math.h>
 
+
+/*
+KNOWN ISSUES:
+searches based on number of grid squares away, which is fine except that if one grid square is smaller than the rest 
+(i.e. when Nx is not a clean mutliple of grid_size) then search range is smaller than it should be.
+
+proposed solutoin:
+Its not ideal, but it would be better to search 1.5 grid square rather than 0.5, as the later manual distance computation will eliminate anything to far away
+*/
 // Override base class with your custom functionality
 
-constexpr int Nx = 960;
-constexpr int Ny = 480;
+constexpr int Nx = 1280;
+constexpr int Ny = 720;
+
+constexpr int num_of_boids = 200;
 constexpr int pixelscale = 1;
 
-const int grid_size = 100;
+const int grid_size = 50;
 const int bar_width = 1;
 int Gx; //number of grid squares across (determined in gen_grid func)
 int Gy;
@@ -50,26 +65,30 @@ private:
 public:
 	Boid(int id) {
 		m_id = id;
+
+		m_size = 2;
 		m_col = olc::BLUE;
 		if (m_id == 0) { m_col = olc::RED; }
 
-		srand(id); //good for debugging, but change this to a proper RNG later
-		m_size = 5;
 		m_max_speed = 100.0f * (0.75f + 0.5f * static_cast<float> (rand()) / static_cast<float> (RAND_MAX)); //give it some fuzz
 
-		//random position
+		//start off with random position + speed
 		olc::vf2d startpos(static_cast <float> (rand()) / static_cast <float> (RAND_MAX / Nx), static_cast <float> (rand()) / static_cast <float> (RAND_MAX / Ny));
-		m_pos = startpos;
-
 		olc::vf2d startvel(static_cast <float> (rand()) / static_cast <float> (RAND_MAX / m_max_speed), static_cast <float> (rand()) / static_cast <float> (RAND_MAX / m_max_speed));
+		startvel = (2 * startvel) - olc::vf2d(m_max_speed, m_max_speed);
+		m_pos = startpos;
 		m_vel = startvel;
+
+
+		m_heading = atan2f(m_vel.y, m_vel.x);
+		m_heading_target = m_heading;
 
 		m_max_force = 0.6;
 		m_max_turnspeed = 3; //about 0.5 rev/s
-		m_repulse_strength = 2400; //might wanna check this, chief
-		m_equil_dist = 4;
+		m_repulse_strength = 80; //might wanna check this, chief
+		m_equil_dist = 2;
 		m_cohesion_strength = m_repulse_strength / m_equil_dist;
-		m_alignment_strength = 1;
+		m_alignment_strength = 0.05;
 
 		m_vision = (float)grid_size;
 
@@ -82,7 +101,35 @@ public:
 		if (m_gy > Gy) { m_gy = 0; }
 
 	}
+	
+	static olc::vf2d rotate(olc::vf2d input, float angle) {
+		olc::vf2d output;
+		output.x = input.x * std::cosf(angle) - input.y * std::sinf(angle);
+		output.y = input.x * std::sinf(angle) + input.y * std::cosf(angle);
+		return output;
+	}
 
+	static std::tuple<float, olc::vf2d> measure_distance(olc::vf2d vec1, olc::vf2d vec2) {
+		//measure distance between points and create normal pointing FROM vec1 TO vec2
+		//(there is a subtlety here due to toroidal world requirements)
+		float best_mag2 = std::numeric_limits<float>::infinity();
+		olc::vf2d best_reflection;
+
+		float reflection_mag2;
+		olc::vf2d reflection;
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				reflection = (vec2 + olc::vf2d(Nx * i, Ny * j)) - vec1; //create nine mirror images and check them all
+				reflection_mag2 = reflection.mag2();
+				if (reflection_mag2 < best_mag2) {
+					best_mag2 = reflection_mag2;
+					best_reflection = reflection;
+				}
+			}
+		}
+		float mag = std::sqrt(best_mag2);
+		return std::make_tuple((float)mag, best_reflection);
+	}
 	static void gen_table(){
 		//take boid_list and apply spatial partitioning
 		Boid::grid_table.clear();
@@ -104,18 +151,15 @@ public:
 			else {
 				Boid::grid_table[key].push_back(ptr); //add to grid
 			}
-			/*if key in grid_table:
-				make new vector
-				insert pointer
-				add to grid_table with key
-			else:
-				add pointer to vector*/
 		}
 	}
 
 	void update(float fElapsedTime) {
 		//update self
 		m_pos += fElapsedTime * m_vel;
+		if (m_accel.mag2() > m_max_force * m_max_force) {
+			m_accel = m_max_force * m_accel.norm();
+		}
 		m_vel += fElapsedTime * m_accel;
 		if (m_vel.mag2() > m_max_speed * m_max_speed) {
 			m_vel = m_max_speed * m_vel.norm();
@@ -132,14 +176,104 @@ public:
 		m_gx = m_pos.x / grid_size;
 		m_gy = m_pos.y / grid_size;
 
-		if (m_id == 0){ std::cout << "G: (" << m_gx << ", " << m_gy << ")\n"; }
+		//rotate by up to max turnspeed*fElapsedTime
+		float rotate;
+		if (std::abs(m_heading_target - m_heading) > m_max_turnspeed) {
+			rotate = m_alignment_strength * m_max_turnspeed * fElapsedTime;
+		}
+		else {
+			rotate = m_alignment_strength * (m_heading_target - m_heading) * fElapsedTime;
+		}
+		rotate = rotate *180 / M_PI;
+		//float rotate = m_alignment_strength * min(m_heading_target - m_heading, m_max_turnspeed * fElapsedTime); //rotation amount
+		m_vel = Boid::rotate(m_vel, rotate); //convert angle to radians before passing to rotate
+		m_heading = atan2f(m_vel.y, m_vel.x);
+		m_heading_target = m_heading;
+		//if (m_id == 0){ std::cout << "G: (" << m_gx << ", " << m_gy << ")\n"; }
 	}
 
-	std::vector<Boid> find_neighbours(int rg = 1){
+	std::vector<Boid*> find_neighbours(int rg){
+		std::vector<Boid*> candidate_neighbours;
+		std::vector<Boid*> neighbours;
+		std::vector<Boid*> res;
 		//get neighbours using grid table
+		std::vector<int> gx_range; //range of gx to check
+		std::vector<int> gy_range; //range of gy to check
+
+		for (int i = m_gx - rg; i < m_gx + rg + 1; i++) { gx_range.push_back(i); }
+		for (int i = m_gy - rg; i < m_gy + rg + 1; i++) { gy_range.push_back(i); }
+		//if ((m_gx == 0) || (m_gx == Gx)) {
+		//	gx_range = { Gx - 1, Gx, 0, 1 }; //i guess this is inefficient if Gx == 1 but in that case spatial partitioning would be pointless anyway so its not worth accounting for
+		//}
+		//else if (m_gx == Gx - 1) {
+		//	gx_range = { Gx - 2, Gx - 1, Gx, 0 };
+		//}
+		//else {
+		//	for (int i = m_gx - 1; i < m_gx + 2; i++) { gx_range.push_back(i); }
+		//}
+		//if ((m_gy == 0)|| (m_gy == Gy)) {
+		//	gy_range = { Gy - 1, Gy, 0, 1 }; //i guess this is inefficient if Gx == 1 but in that case spatial partitioning would be pointless anyway so its not worth accounting for
+		//}
+		//else if (m_gy == Gy - 1) {
+		//	gy_range = { Gy - 2, Gy - 1, Gy, 0 };
+		//}
+		//else {
+		//	for (int i = m_gy - rg; i < m_gy + rg + 1; i++) { gy_range.push_back(i); }
+		//}
+
+		std::pair<int, int> key;
+		for (std::vector<int>::iterator i = gx_range.begin(); i != gx_range.end(); i++) {
+			for (std::vector<int>::iterator j = gy_range.begin(); j != gy_range.end(); j++) {
+				key = std::make_pair( *i, *j );
+				res = Boid::grid_table[key]; //this will create empty vecs everywhere but maybe thats okay?
+				candidate_neighbours.insert(candidate_neighbours.end(), res.begin(), res.end());
+			}
+		}
+
+		//now check their ACTUAL distance and ID numbers to prune as necessary
+		for (int i = 0; i < candidate_neighbours.size(); i++) {
+			//deleting elements from the middle of vectors is an inefficient operation, so we create 
+			//a new vector instead and then discard the old candidates naturally when the scope is exited
+			olc::vf2d candidate_pos = (*candidate_neighbours[i]).m_pos;
+			int candidate_id = (*candidate_neighbours[i]).m_id;
+
+		if ((candidate_id != m_id) && ( std::get<0>(Boid::measure_distance(candidate_pos, m_pos))< m_vision)) {
+				neighbours.push_back(candidate_neighbours[i]);
+				//add to list
+			}
+		}
+		//if (m_id == 0) { std::cout << neighbours.size() << " neighbours" << std::endl; }
+		/* We need to search both +/- rg as a Moore radius, remembering to clamp any rollover back to 0*/
+		return neighbours;
 	}
 
-	void calculate_movement() {
+	void calculate_movement(float fElapsedTime) {
+		std::vector<Boid*> neighbours = find_neighbours((int) (m_vision/grid_size) + 1);
+		olc::vf2d avg_pos(0, 0);
+		float avg_heading = 0;
+		bool exist_neighbours = false;
+		for (int i = 0; i < neighbours.size(); i++) {
+			exist_neighbours = true;
+			Boid* neighbour = neighbours[i];
+			//draw blue lines here if you want
+			//apply repulsion
+			float mag;
+			olc::vf2d norm;
+			std::tie(mag, norm) = Boid::measure_distance((*neighbour).m_pos, m_pos);
+			m_accel += m_repulse_strength  * norm / mag;
+			//apply cohesion
+			avg_pos += (*neighbour).m_pos;
+			//align
+			avg_heading += (*neighbour).m_heading;
+		}
+		if (exist_neighbours) {
+			olc::vf2d cohesion_target = avg_pos / neighbours.size();
+			float mag;
+			olc::vf2d normal;
+			std::tie(mag, normal) = Boid::measure_distance(m_pos, cohesion_target);
+			m_accel += m_cohesion_strength * normal;
+			m_heading_target = avg_heading / neighbours.size();
+		}
 		//m_accel = olc::vf2d(0, -10);
 		//pass
 	}
@@ -161,38 +295,13 @@ public:
 public:
 	void place_agents() {
 		//temp debug boids
-		Boid loner(0);
-		loner.m_pos = olc::vf2d(0, Ny / 2);
-		loner.m_vel = olc::vf2d(40,0);
-		Boid::boid_list.push_back(loner);
-
-		Boid pal(1);
-		pal.m_pos = olc::vf2d(Nx, Ny / 2);
-		pal.m_vel = olc::vf2d(-40, 0);
-		Boid::boid_list.push_back(pal);
-		
+		srand(0); //good for debugging, but change this to a proper RNG later
+		for (int i = 0; i < num_of_boids; i++) {
+			Boid agent(i);
+			Boid::boid_list.push_back(agent);
+		}	
 	}
 
-	std::tuple<float, olc::vf2d> measure_distance(olc::vf2d vec1, olc::vf2d vec2) {
-		//measure distance between points and create normal pointing FROM vec1 TO vec2
-		//(there is a subtlety here due to toroidal world requirements)
-		float best_mag2 = std::numeric_limits<float>::infinity();
-		olc::vf2d best_reflection;
-
-		float reflection_mag2;
-		olc::vf2d reflection;
-		for (int i = -1; i < 2; i++) {
-			for (int j = -1; j < 2; j++) {
-				reflection = (vec2 + olc::vf2d(Nx * i, Ny * j)) - vec1; //create nine mirror images and check them all
-				reflection_mag2 = reflection.mag2();
-				if (reflection_mag2 < best_mag2) {
-					best_mag2 = reflection_mag2;
-					best_reflection = reflection;
-				}
-			}
-		}
-		return std::make_tuple((float)std::sqrt(best_mag2), reflection);
-	}
 	bool OnUserCreate() override
 	{
 		Gx = Nx / grid_size;
@@ -218,17 +327,11 @@ public:
 
 		for (int i = 0; i < Boid::boid_list.size(); i++) {
 			Boid* candidate = &Boid::boid_list[i];
-			(*candidate).calculate_movement();
+			(*candidate).calculate_movement(fElapsedTime);
 			(*candidate).update(fElapsedTime);
-			FillCircle((*candidate).m_pos, (*candidate).m_size, (*candidate).m_col);
-			//LOG((*candidate).m_pos);
-			//draw_boid()
-
+			FillCircle((*candidate).m_pos, (*candidate).m_size, (*candidate).m_col); //draw boid
 		}
-		// called once per frame, draws random coloured pixels
-		/*for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));*/
+
 		return true;
 	}
 };
